cecac83083c0183cad76b24b0898e7cd2053eaab
Neal Geilen <44571222+NealGeilen@users.noreply.github.com>
2021-12-11 16:54:52 +0100

    feat: added Context

---
+++ API/Data/QsContext.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public interface IQsContext
+    {
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DataPoint> DataPoints { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+    public class QsContext :  DbContext, IQsContext
+    {
+        public QsContext(DbContextOptions<QsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DataPoint> DataPoints { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
