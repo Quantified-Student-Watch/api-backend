4f4eb9f5c237afd561b62e5c1b0c0ae18dd79b4b
Neal Geilen <44571222+NealGeilen@users.noreply.github.com>
2021-12-12 19:36:13 +0100

    feat: added ensure create database on application start up

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,15 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QsContext>(o =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+                if (connectionString != null)
+                {
+                    o.UseSqlServer(connectionString,
+                        sqlOptions => sqlOptions.EnableRetryOnFailure(50, TimeSpan.FromSeconds(30), null));
+                }
+            });
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
         }
@@ -39,6 +50,11 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+            
+            using (var scope = 
+                   app.ApplicationServices.CreateScope())
+            using (var context = scope.ServiceProvider.GetService<QsContext>())
+                context?.Database.EnsureCreated();
 
             app.UseHttpsRedirection();
 
