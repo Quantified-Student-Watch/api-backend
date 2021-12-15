f4451a03e1d34f4c383707484c8c52519bf29147
Neal Geilen <44571222+NealGeilen@users.noreply.github.com>
2021-12-15 09:26:32 +0100

    Update

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("user/")]
+    public class UserController
+    {
+        public UserController(IUser)
+        {
+            
+        }
+        
+        
+        
+        [HttpGet("{id}")]
+        public User GetUser(Guid id)
+        {
+            return OkResult()
+        }
+
+
+    }
+}
